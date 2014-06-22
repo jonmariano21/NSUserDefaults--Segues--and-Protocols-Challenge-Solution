9002704cc0507b7899db730961df7a00c049b0ad
jonmariano21 <jonmariano21@gmail.com>
2014-06-21 18:48:27 -0700

    Initial Commit

---
+++ NSUserDefaults, Segues, and Protocols Challenge Solution/JMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JMAppDelegate.h
+//  NSUserDefaults, Segues, and Protocols Challenge Solution
+//
+//  Created by JONATHAN MARIANO on 6/21/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSUserDefaults, Segues, and Protocols Challenge Solution/JMViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMViewController.h
+//  NSUserDefaults, Segues, and Protocols Challenge Solution
+//
+//  Created by JONATHAN MARIANO on 6/21/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMViewController : UIViewController
+
+@end
